4ea648e6fa48b8d0e19c5387cc2b4d72fb9ecc98
Simão Freire <simaofreire@live.com>
2024-04-22 19:36:18 -0300

    First commit

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+import cookie from '@fastify/cookie'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
+
+const app = fastify()
+
+app.register(cookie)
+
+app.register(transactionsRoutes, {
+  prefix: '/transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Server is running on port 3333')
+  })
